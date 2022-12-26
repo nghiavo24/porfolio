a8e3553c0b461c3a28ac4412b8b34dde9b2c431d
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-26 01:48:34 -0600

    Completed About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,12 +4,20 @@ const About = () => {
   return (
     <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
         <div className='flex flex-col justify-center items-center w-full h-full'>
-            <div>
-                <p>About</p>
-            </div>
-            <div>
-                
-            </div>
+            <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+                <div className='sm:text-right pb-8 pl-4'>
+                    <p className='text-4xl font-bold inline border-b-4 border-pink-600'>About</p>
+                </div>
+                    <div></div>
+                    </div>
+                <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+                    <div className='sm:text-right text-4xl font-bold '>
+                        <p>Hi. I'm Nathan. Nice to meet you. Please take a look around</p>
+                    </div>
+                    <div>
+                        <p>My background in Healthcare industry and my experience in serving a variety of customers with different background enables me to have a strong foundation in communication, collaboration in teams efficiently and I have also learned how to lead a team. I am passionate about improving or creating applications that impact people in a positive way, whether it is through Healthcare or simply making that application more accessible. </p>
+                    </div>
+                </div>
         </div>
     </div>
   )
