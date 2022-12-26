038f32a736fcf814c4d156a9dcb559c6e74925cf
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-26 01:28:24 -0600

    Add About component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import About from './components/About';
 import { Home } from './components/Home';
 import Navbar from './components/Navbar';
 
@@ -8,6 +9,7 @@ const App = () => {
     <div>
       <Navbar />
       <Home />
+      <About />
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,10 +9,10 @@ export const Home = () => {
             <p className='text-pink-600'>Hi, my name is</p>
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Nghia Vo</h1>
             <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer.</h2>
-            <p className='text-[8892b0] py-4 max-w-[700px]'>I'm a full-stack developer that want to make, design and build products and ideas that matter and have a positive impact in every day life.</p>
+            <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a full-stack developer that want to make, design and build products and ideas that matter and have a positive impact in every day life.</p>
             <div>
                 <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:border-pink-600 hover:bg-pink-600'>
-                    More About Me
+                    About Me
                         <span className='group-hover:rotate-90 duration-300'>
                     <HiArrowNarrowRight className='ml-3' />
                         </span>
