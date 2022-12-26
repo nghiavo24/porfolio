6300b336dcef798fca7fc3c81829d0dcc59c8ccf
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-26 02:51:26 -0600

    Completed Skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,6 +6,9 @@ import tailwind from '../assets/tailwind.png';
 import css from '../assets/css.png';
 import node from '../assets/node.png';
 import mongodb from '../assets/mongo.png';
+import python from '../assets/python.png'
+import git from '../assets/git.png';
+import postgres from '../assets/postgresql.svg';
 
 const skills = [
     {
@@ -21,62 +24,71 @@ const skills = [
         style: 'shadow-blue-500'
     },
     {
-        id: 1,
-        src: html,
-        title: 'HTML',
-        style: 'shadow-orange-500'
+        id: 3,
+        src: node,
+        title: 'Node',
+        style: 'shadow-lime-300'
     },
     {
-        id: 3,
+        id: 4,
         src: javascript,
         title: 'JavaScript',
         style: 'shadow-yellow-500'
     },
     {
-        id: 4,
+        id: 5,
         src: react,
         title: 'React',
         style: 'shadow-blue-600'
     },
     {
-        id: 5,
+        id: 6,
         src: tailwind,
         title: 'Tailwind',
         style: 'shadow-sky-400'
     },
     {
-        id: 6,
-        src: node,
-        title: 'Node',
-        style: 'shadow-green-500'
+        id: 7,
+        src: postgres,
+        title: 'PostgresQL',
+        style: 'shadow-gray-100'
     },
     {
-        id: 7,
+        id: 8,
         src: mongodb,
         title: 'MongoDB',
-        style: 'shadow-brown-500'
+        style: 'shadow-green-500'
     }, 
     {
-        id: 8,
+        id: 9,
         src: python,
         title: 'Python',
         style: 'shadow-yellow-400'
-    }
+    },
+    {
+        id: 10,
+        src: git,
+        title: 'Git',
+        style: 'shadow-orange-600'
+    },
 ]
 
 const Skills = () => {
   return (
-    <div name='skills' className='bg-[#0a192f]'>
-        <div>
+    <div name='skills' className='bg-[#0a192f] text-gray-300'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div>
-                <p className='text-4xl text-gray-300 py-4 font-bold inline border-pink-600'>Experience</p>
-                <p className='py-4'>These </p>
+                <p className='text-4xl text-gray-300 py-4 font-bold inline border-pink-600'>Skills</p>
+                <p className='py-4'>These are the technologies I've worked with</p>
             </div>
-            <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
-                <div className='shadow-md hover:scale-105 duration-500 py-2 rounded-lg'>
-                    <img src={html} alt='HTML icon' className='w-20 mx-auto' />
-                    <p className='mt-4'>HTML</p>
+            <div className='w-full grid grid-cols-2 sm:grid-cols-5 gap-8 text-center py-8 px-12 sm:px-0'>
+               
+               {skills.map(({id, src, title, style}) => (
+                    <div key={id} className={`shadow-md hover:scale-110 duration-500 py-2 rounded-lg ${style}`}>
+                    <img src={src} alt='{title}' className='w-20 mx-auto' />
+                    <p className='mt-4'>{title}</p>
                 </div>
+                ))}
             </div>
         </div>
     </div>
