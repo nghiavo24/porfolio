fafd2e28359307f898fbee29e8db643dccafa42c
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-26 00:45:08 -0600

    basic Navbar set up, conditional rendering worked

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState} from 'react'
 import {FaBars, FaTimes} from 'react-icons/fa'
 
 const Navbar = () => {
+    const[nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>Navbar
         <div>
@@ -14,11 +16,11 @@ const Navbar = () => {
             <li>Projects</li>
             <li>Contact</li>
             </ul>
-        <div className='md:hidden'>
-            <FaBars />
+        <div className='md:hidden z-10' onClick={handleClick}>
+            {!nav ? <FaBars /> : <FaTimes />}
         </div>
         
-        <ul className='absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'>
+        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
             <li className='py-6 text-4xl'>Home</li>
             <li className='py-6 text-4xl'>About</li>
             <li className='py-6 text-4xl'>Skills</li>
