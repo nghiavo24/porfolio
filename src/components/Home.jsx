b950fae2570c7323cc7b100edf29ac95488e52bc
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-26 01:18:16 -0600

    Home component completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,22 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 export const Home = () => {
   return (
-    <div className='w-full h-screen bg-[#0a192f]'>
-        Home
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+        
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <p className='text-pink-600'>Hi, my name is</p>
+            <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Nghia Vo</h1>
+            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer.</h2>
+            <p className='text-[8892b0] py-4 max-w-[700px]'>I'm a full-stack developer that want to make, design and build products and ideas that matter and have a positive impact in every day life.</p>
+            <div>
+                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:border-pink-600 hover:bg-pink-600'>
+                    More About Me
+                        <span className='group-hover:rotate-90 duration-300'>
+                    <HiArrowNarrowRight className='ml-3' />
+                        </span>
+                    </button>
+            </div>
         </div>
+    </div>
   )
 }
