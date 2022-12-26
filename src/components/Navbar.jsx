3b9fcd66abcb65e2fb906ed921cf34944a9924ea
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-26 00:59:17 -0600

    Added the rest of info on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,12 +29,30 @@ const Navbar = () => {
             <li className='py-6 text-4xl'>Projects</li>
             <li className='py-6 text-4xl'>Contact</li>
         </ul>
-        <div className='flex fixed flex-col top-[35%] left-0'>
+        <div className='hidden md:flex fixed flex-col top-[35%] left-0'>
     <ul>
         <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a className='flex justify-between items-center w-full text-gray-300'
             href=''>
                 Linkedin <FaLinkedin size={30}/>
+            </a>
+        </li>
+        <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#33333]'>
+            <a className='flex justify-between items-center w-full text-gray-300'
+            href=''>
+                Github <FaGithub size={30}/>
+            </a>
+        </li>
+        <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+            <a className='flex justify-between items-center w-full text-gray-300'
+            href=''>
+                Email <HiOutlineMail size={30}/>
+            </a>
+        </li>
+        <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+            <a className='flex justify-between items-center w-full text-gray-300'
+            href=''>
+                Resume <BsFillPersonLinesFill size={30}/>
             </a>
         </li>
     </ul>
