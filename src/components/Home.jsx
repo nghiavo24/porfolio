7c5182c8c80d5ca7fb36c507f1cac3784b443f55
nghiavo24 <nghia.vo1452@gmail.com>
2023-01-08 04:13:20 -0600

    Added some styling on homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,11 +21,11 @@ export const Home = () => {
   return (
     <div name='home' className='w-full h-screen bg-[#0a192f] md:px-16'>
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-            <p className='text-pink-600 text-xl'>Hi, my name is</p>
-            <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Nghia Vo</h1>
-            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a full-stack developer.</h2>
-            <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a full-stack developer that want to make, design and build products and ideas that matter and have a positive impact in every day life.</p>            
-            <div className='text-xl'>
+            <p className='text-pink-600 text-xl pb-2'>Hi, my name is</p>
+            <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Nathan Vo</h1>
+            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0] py-2'>I'm a full-stack developer.</h2>
+            <p className='text-[#8892b0] py-4 max-w-[700px] text-lg'>I'm a full-stack developer & a life-long learner that want to make, design and build products and ideas that matter and have a positive impact in every day life.</p>            
+            <div className='text-xl py-4'>
               <p className='text-[#ccd6f6]'>
                 <span>{text}</span>
               </p>
