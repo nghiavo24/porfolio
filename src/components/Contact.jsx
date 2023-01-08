d7e881f36a88a2d390bb90bdc9e55a1d21963895
nghiavo24 <nghia.vo1452@gmail.com>
2023-01-08 03:45:45 -0600

    Fixed button in Contact and require form to have at least msg to submit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,8 +10,10 @@ const Contact = () => {
             </div>
             <input className='bg-[#ccd6f6] p-2' type='text' placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#ccd6f6]' type='email' placeholder='Email' name='email' />
-            <textarea className='bg-[#ccd6f6] p-2' rows='7' type='message' placeholder='Message' name='message' />
-            <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-4 my-8 mx-auto flex items-center'>Let's Collaborate</button>
+            <textarea className='bg-[#ccd6f6] p-2' rows='7' type='message' placeholder='Message' name='message' required/>
+            <div className='grid gap-8 items-start justify-center py-4'>
+              <button className='text-white bg-gradient-to-r from-purple-700 to-pink-700 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-xl px-6 py-4 flex items-center'>Let's Collaborate❗</button>
+            </div>
         </form>
     </div>
   )
