d82c05fae971babb3829d6f164598754e66932f2
nghiavo24 <nghia.vo1452@gmail.com>
2023-01-08 03:03:21 -0600

    Add logo to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import React, { useState} from 'react';
 import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa';
 import {HiOutlineMail} from 'react-icons/hi';
-import {BsFillPersonLinesFill} from 'react-icons/bs'
-import {Link} from 'react-scroll'
+import {BsFillPersonLinesFill} from 'react-icons/bs';
+import {Link} from 'react-scroll';
+import Logo from '../assets/logo_initials.png'
 
 const Navbar = () => {
     const[nav, setNav] = useState(false)
     const handleClick = () => setNav(!nav)
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>Navbar
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
-            <img />
+            <img src={Logo} alt="Logo Image" className='w-3/12 pt-2' />
         </div>
             <ul className='hidden md:flex'>
             <li><Link  to="home" smooth={true} duration={500}>Home</Link></li>
