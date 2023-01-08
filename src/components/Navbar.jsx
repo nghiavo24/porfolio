321bfee038123dbe15833cd407db2a21d3d2663b
nghiavo24 <nghia.vo1452@gmail.com>
2023-01-08 03:04:08 -0600

    Fixed color on left bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -64,7 +64,7 @@ const Navbar = () => {
                 Linkedin <FaLinkedin size={30}/>
             </a>
         </li>
-        <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#33333]'>
+        <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-black'>
             <a className='flex justify-between items-center w-full text-gray-300'
             href='https://github.com/nghiavo24'>
                 Github <FaGithub size={30}/>
