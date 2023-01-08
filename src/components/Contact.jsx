1509cd01f86b361b331e03b013506bb0bbd6480e
nghiavo24 <nghia.vo1452@gmail.com>
2023-01-08 04:05:18 -0600

    Fixed contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-        <form method='POST' action='https://getform.io/f/55bb6a98-4c36-4302-8c27-57c0f3ef92b7' className='flex flex-col max-w-[600px] w-full'>
-            <div className='pb-8'>
+        <form method='POST' action='https://getform.io/f/55bb6a98-4c36-4302-8c27-57c0f3ef92b7' className='flex flex-col max-w-xl w-full'>
+            <div className='pb-6'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'>Submit the form below or shoot me an email - nghia.vo1452@gmail.com</p>
+                <p className='text-gray-300 text-xl py-6'>Submit the form below or click <a href="mailto:nghia.vo1452@gmail.com"><span className='underline underline-offset-2 hover:text-pink-600'>here</span></a> to send me an email.</p>
             </div>
             <input className='bg-[#ccd6f6] p-2' type='text' placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#ccd6f6]' type='email' placeholder='Email' name='email' />
