c8aa4a3f49051df154d6d6371889a73b977e48aa
nghiavo24 <nghia.vo1452@gmail.com>
2023-01-08 06:36:27 -0600

    Fixed some styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
         <form method='POST' action='https://getform.io/f/55bb6a98-4c36-4302-8c27-57c0f3ef92b7' className='flex flex-col max-w-xl w-full'>
             <div className='pb-6'>
-                <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
+                <h1 className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</h1>
                 <p className='text-gray-300 text-xl py-6'>Submit the form below or click <a href="mailto:nghia.vo1452@gmail.com"><span className='underline underline-offset-2 hover:text-pink-600'>here</span></a> to send me an email.</p>
             </div>
             <input className='bg-[#ccd6f6] p-2' type='text' placeholder='Name' name='name' />
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -40,9 +40,9 @@ const Project = () => {
     <div name='project' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
-          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
+          <h1 className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
             Project
-          </p>
+          </h1>
           <p className='text-2xl py-6'>Check out some of my recent projects</p>
         </div>
 <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
