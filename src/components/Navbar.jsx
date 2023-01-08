89e78b3ebe25b90ce855ba248c355f0e9f924f1d
nghiavo24 <nghia.vo1452@gmail.com>
2023-01-08 04:48:30 -0600

    Redesign hamburger menu icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,22 @@ const Navbar = () => {
             </ul>
 
         <div className='md:hidden z-10 '  onClick={handleClick}>
-            {!nav ? <FaBars style={{ width: '2rem', height: 'auto'}} /> : <FaTimes style={{ width: '2rem', height: 'auto'}}/>}
+          <div>
+            <button class="relative group">
+              <div class="relative flex overflow-hidden items-center justify-center rounded-full w-[50px] h-[50px] transform transition-all ring-0 ring-gray-300 hover:ring-8 group-focus:ring-4 ring-opacity-30 duration-200 shadow-md">
+                <div class="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center overflow-hidden">
+                  <div class="bg-white h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:translate-y-6 delay-100"></div>
+                  <div class="bg-white h-[2px] w-7 rounded transform transition-all duration-300 group-focus:translate-y-6 delay-75"></div>
+                  <div class="bg-white h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:translate-y-6"></div>
+
+                  <div class="absolute items-center justify-between transform transition-all duration-500 top-2.5 -translate-x-10 group-focus:translate-x-0 flex w-0 group-focus:w-12">
+                    <div class="absolute bg-white h-[2px] w-5 transform transition-all duration-500 rotate-0 delay-300 group-focus:rotate-45"></div>
+                    <div class="absolute bg-white h-[2px] w-5 transform transition-all duration-500 -rotate-0 delay-300 group-focus:-rotate-45"></div>
+                  </div>
+                </div>
+              </div>
+            </button>
+          </div>
         </div>
         
         <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
