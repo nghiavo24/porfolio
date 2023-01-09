c94a7a0a8413f73639ad88eb2145f017f15d99ca
nghiavo24 <nghia.vo1452@gmail.com>
2023-01-09 04:30:15 -0600

    Added some styling on project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Project1 from '../assets/Project1.png';
+import { AiFillEye as Live, AiFillGithub as Github } from 'react-icons/ai'
 
 const Project = () => {   
     const projects =[
         {
             id:1,
-            name:"React JS Application",
-            image:'',
-            github:"",
-            live:"",
+            name:"The Dev Hub",
+            image: Project1,
+            github:"https://github.com/nghiavo24/the-dev-hub-fe",
+            live:"https://the-dev-hub.vercel.app/",
+            description: "Using MERN stack"
         },
         {
             id:2,
@@ -15,6 +18,7 @@ const Project = () => {
             image:'',
             github:"",
             live:"",
+            description: ""
         },
         {
             id:3,
@@ -22,6 +26,7 @@ const Project = () => {
             image:'',
             github:"",
             live:"",
+            description: ""
         },
         {
             id:4,
@@ -29,6 +34,7 @@ const Project = () => {
             image:'',
             github:"",
             live:"",
+            description: ""
         },
     
     ]
@@ -43,40 +49,42 @@ const Project = () => {
           </h1>
           <p className='text-2xl py-6'>Check out some of my recent projects</p>
         </div>
-<div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {projects.map((item, index) => (
-  <div
-    key={index}
-    style={{ backgroundImage: `url(${item.image})` }}
-    className="shadow-lg shadow-[#040c16] group container rounded-md 
-              flex justify-center text-center items-center mx-auto content-div "
-  >
-    {/* Hover effect for images */}
-    <div className="opacity-0 group-hover:opacity-100 ">
-      <span className="text-2xl font bold text-white tracking-wider ">
-        {item.name}
-      </span>
-      <div className="pt-8 text-center ">
-        {/* eslint-disable-next-line */}
-        <a href={item.github} target="_blank">
-          <button
-            className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-            Code
-          </button>
-        </a>
-        {/* eslint-disable-next-line */}
-        <a href={item.live} target="_blank">
-          <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-            Live
-          </button>
-        </a>
-      </div>
-    </div>
-  </div>
-))}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+                  {projects.map((item, index) => (
+            <div
+              key={index}
+              style={{ backgroundImage: `url(${item.image})` }}
+              className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div ">
+              {/* Hover effect for images */}
+              <div className="opacity-0 group-hover:opacity-100 ">
+                  <span className="text-xl text-white tracking-wider ">
+                    {item.name}
+                  </span>
+                  <br />
+                  <span>
+                    {item.description}
+                  </span>
+                  <div className="pt-8 text-center">
+                    {/* eslint-disable-next-line */}
+                    <a href={item.live} target="_blank">
+                      <button
+                        className="text-center rounded-lg px-2 py-2 m-1 bg-white text-gray-700 font-bold text-lg">
+                        <Live />
+                      </button>
+                    </a>
+                    {/* eslint-disable-next-line */}
+                    <a href={item.github} target="_blank">
+                      <button className="text-center rounded-lg px-2 py-2 m-1 bg-white text-gray-700 font-bold text-lg">
+                        <Github />
+                      </button>
+                    </a>
+                </div>
+              </div>
+                <div classname="inline"></div>
+            </div>
+          ))}
 
-
-</div>
+          </div>
       </div>
     </div>
   );
