c5a4f83de8fcea4ed6be8a3466d98167e048d8eb
nghiavo24 <nghia.vo1452@gmail.com>
2023-01-09 04:47:39 -0600

    Add animation on mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -56,19 +56,19 @@ const Navbar = () => {
         </m.div>
         
         <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-            <li className='py-6 text-4xl hover:text-pink-600'>
+            <li className='py-6 text-4xl hover:text-pink-600 hover:animate-headShake'>
               <Link onClick={handleClick} to='home' smooth={true} duration={500}>Home</Link>
             </li>
-            <li className='py-6 text-4xl hover:text-pink-600'>
+            <li className='py-6 text-4xl hover:text-pink-600 hover:animate-headShake'>
               <Link onClick={handleClick} to='about' smooth={true} duration={500}>About</Link>
             </li>
-            <li className='py-6 text-4xl hover:text-pink-600'>
+            <li className='py-6 text-4xl hover:text-pink-600 hover:animate-headShake'>
               <Link onClick={handleClick} to='skill' smooth={true} duration={500}>Skills</Link>
             </li>
-            <li className='py-6 text-4xl hover:text-pink-600'>
+            <li className='py-6 text-4xl hover:text-pink-600 hover:animate-headShake'>
               <Link onClick={handleClick} to='project' smooth={true} duration={500}>Projects</Link>
             </li>
-            <li className='py-6 text-4xl hover:text-pink-600'>
+            <li className='py-6 text-4xl hover:text-pink-600 hover:animate-headShake'>
               <Link onClick={handleClick} to='contact' smooth={true} duration={500}>Contact</Link>
             </li>
         </ul>
