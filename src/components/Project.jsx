bec346d726a59da00d48343d08301be71498e81c
nghiavo24 <nghia.vo1452@gmail.com>
2023-01-10 01:01:47 -0600

    Added DarkMode to Project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -21,7 +21,7 @@ const Project = () => {
             image: Project2,
             github:"https://github.com/nghiavo24/online-store-fe",
             live:"https://online-store-pt8ruqtva-guillermoaviles.vercel.app/",
-            description: "An online store where users can buy and sell items. The web application was built using MERN technology and it has full CRUD actions."
+            description: "An online store where users can buy and sell items. The application was built using MERN stack technologies and it has full CRUD actions."
         },
         {
             id:3,
@@ -29,7 +29,7 @@ const Project = () => {
             image: Project3,
             github:"https://github.com/nghiavo24/game-of-thrones",
             live:"https://game-of-thrones-nu.vercel.app/",
-            description: "A mini-wikipedia about game show Game of Thrones. It is a practice using Axios to request data from 3rd-party API and presenting it. It is also a deep dive into React as a JS framework. "
+            description: "A mini-wikipedia about game show Game of Thrones. It is a deep dive into ReactJS and parsing data from 3rd-part API to present it effectively. "
         },
         {
             id:4,
@@ -44,10 +44,10 @@ const Project = () => {
     
   
   return (
-    <div name='project' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+    <div name='project' className='w-full md:h-screen text-black bg-[#DDD4D0] dark:text-gray-300 dark:bg-[#211a1d]'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pt-52 2xl:pt-0'>
-          <h1 className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
+          <h1 className='text-4xl font-bold inline border-b-4 dark:text-gray-300 border-[#F08080] dark:border-pink-600'>
             Projects
           </h1>
           <p className='text-2xl py-6'>Check out some of my recent projects</p>
@@ -57,21 +57,20 @@ const Project = () => {
             <div
               key={index}
               style={{ backgroundImage: `url(${item.image})` }}
-              className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div ">
+              className="shadow-lg shadow-[#CF9893] dark:shadow-[#7a7a7a] group container rounded-md flex justify-center text-center items-center mx-auto content-div ">
               {/* Hover effect for images */}
               <div className="opacity-0 group-hover:opacity-100 ">
-                  <span className="text-xl text-white tracking-wider ">
+                  <span className="text-xl dark:text-white tracking-wider ">
                     {item.name}
                   </span>
                   <br />
-                  <span>
+                  <span className='italic text-base'>
                     {item.description}
                   </span>
                   <div className="pt-8 text-center">
                     {/* eslint-disable-next-line */}
                     <a href={item.live} target="_blank">
-                      <button
-                        className="text-center rounded-lg px-2 py-2 m-1 bg-white text-gray-700 font-bold text-lg">
+                      <button className="text-center rounded-lg px-2 py-2 m-1 bg-white text-gray-700 font-bold text-lg">
                         <Live />
                       </button>
                     </a>
@@ -83,7 +82,6 @@ const Project = () => {
                     </a>
                 </div>
               </div>
-                <div classname="inline"></div>
             </div>
           ))}
 
