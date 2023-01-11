44791ebddebddcfa247afb96a5ef0f3d00de4247
nghiavo24 <nghia.vo1452@gmail.com>
2023-01-10 20:05:39 -0600

    Updated Linkedin link on Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -87,7 +87,7 @@ const Navbar = () => {
     >
         <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#003f88] dark:bg-blue-600 rounded-lg'>
             <a className='flex justify-between items-center w-full text-gray-300'
-            href='https://www.linkedin.com/in/nghia-vo/'target='_blank' rel='noreferrer'>
+            href='https://www.linkedin.com/in/nathaniel-vo/'target='_blank' rel='noreferrer'>
                 Linkedin <FaLinkedin size={30} />
             </a>
         </li>
