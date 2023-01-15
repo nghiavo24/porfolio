e9f3047d743d6e73f63d198d2cb89bbf47376ffa
nghiavo24 <nghia.vo1452@gmail.com>
2023-01-15 00:00:36 -0600

    Match bg color of navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -52,13 +52,13 @@ const Navbar = () => {
             <button className="relative group" onClick={handleClick}>
               <div className="relative flex overflow-hidden items-center justify-center rounded-full w-[50px] h-[50px] transform transition-all ring-0 ring-black hover:ring-5 group-focus:ring-4 dark:ring-gray-300 dark:hover:ring-8 dark:group-focus:ring-4 ring-opacity-75 dark:ring-opacity-30 duration-200 shadow-md">
                 <div className="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center overflow-hidden">
-                  <div className="bg-black dark:bg-white h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:translate-y-6 delay-100"></div>
-                  <div className="bg-black dark:bg-white h-[2px] w-7 rounded transform transition-all duration-300 group-focus:translate-y-6 delay-75"></div>
-                  <div className="bg-black dark:bg-white h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:translate-y-6"></div>
+                  <div className="bg-[#211A1D] dark:bg-white h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:translate-y-6 delay-100"></div>
+                  <div className="bg-[#211A1D] dark:bg-white h-[2px] w-7 rounded transform transition-all duration-300 group-focus:translate-y-6 delay-75"></div>
+                  <div className="bg-[#211A1D] dark:bg-white h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:translate-y-6"></div>
 
                   <div className="absolute items-center justify-between transform transition-all duration-500 top-2.5 -translate-x-10 group-focus:translate-x-0 flex w-0 group-focus:w-12">
-                    <div className="absolute bg-black dark:bg-white h-[2px] w-5 transform transition-all duration-500 rotate-0 delay-300 group-focus:rotate-45"></div>
-                    <div className="absolute bg-black dark:bg-white h-[2px] w-5 transform transition-all duration-500 -rotate-0 delay-300 group-focus:-rotate-45"></div>
+                    <div className="absolute bg-[#211A1D] dark:bg-white h-[2px] w-5 transform transition-all duration-500 rotate-0 delay-300 group-focus:rotate-45"></div>
+                    <div className="absolute bg-[#211A1D] dark:bg-white h-[2px] w-5 transform transition-all duration-500 -rotate-0 delay-300 group-focus:-rotate-45"></div>
                   </div>
                 </div>
               </div>
