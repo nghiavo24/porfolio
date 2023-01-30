6fb53f6b7b0114afa962f845d2ca983d5a8b35dd
nghiavo24 <nghia.vo1452@gmail.com>
2023-01-30 13:51:53 -0600

    Moved skills data for organization purpose

---
+++ src/data/skills.jsx
@@ -0,0 +1,76 @@
+import html from '../assets/html.png';
+import javascript from '../assets/javascript.png';
+import react from '../assets/react.png';
+import tailwind from '../assets/tailwind.png';
+import css from '../assets/css.png';
+import node from '../assets/node.png';
+import mongodb from '../assets/mongo.png';
+import python from '../assets/python.png'
+import git from '../assets/git.png';
+import postgres from '../assets/postgresql.svg';
+
+const skills = [
+    {
+        id: 1,
+        src: html,
+        title: 'HTML',
+        style: 'shadow-orange-500'
+    },
+    {
+        id: 2,
+        src: css,
+        title: 'CSS',
+        style: 'shadow-blue-500'
+    },
+    {
+        id: 3,
+        src: node,
+        title: 'Node',
+        style: 'shadow-lime-300'
+    },
+    {
+        id: 4,
+        src: javascript,
+        title: 'JavaScript',
+        style: 'shadow-yellow-400'
+    },
+    {
+        id: 5,
+        src: react,
+        title: 'React',
+        style: 'shadow-[#15D7FD]'
+    },
+    {
+        id: 6,
+        src: tailwind,
+        title: 'Tailwind',
+        style: 'shadow-[#45A8B3]'
+    },
+    {
+        id: 7,
+        src: python,
+        title: 'Python',
+        style: 'shadow-yellow-400'
+    },
+    {
+        id: 8,
+        src: postgres,
+        title: 'PostgresQL',
+        style: 'shadow-gray-100'
+    },
+    {
+        id: 9,
+        src: mongodb,
+        title: 'MongoDB',
+        style: 'shadow-green-500'
+    }, 
+    {
+        id: 10,
+        src: git,
+        title: 'Git',
+        style: 'shadow-orange-600'
+    }
+    
+]
+
+export default skills
