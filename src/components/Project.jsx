e466d48a8b45b70b2d7fed2180cd2ad1523b348b
nghiavo24 <nghia.vo1452@gmail.com>
2023-01-30 13:47:34 -0600

    Moved Project data to another file

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,48 +1,8 @@
 import React from 'react';
-import Project1 from '../assets/Project1.png';
-import Project2 from '../assets/Project2.png';
-import Project3 from '../assets/Project3.png';
-import Project4 from '../assets/Project4.png';
 import { AiFillEye as Live, AiFillGithub as Github } from 'react-icons/ai'
+import projects from '../data/projects';
 
 const Project = () => {   
-    const projects =[
-        {
-            id:1,
-            name:"The Dev Hub",
-            image: Project1,
-            github:"https://github.com/the-dev-hub-project",
-            live:"https://the-dev-hub.vercel.app/",
-            description: "Organization tool to facilitate job search. The app has full CRUD actions and equiped with Firebase authentication and styling was done using Tailwind."
-        },
-        {
-            id:2,
-            name:"Dealio",
-            image: Project2,
-            github:"https://github.com/Dealio-Dev-Nathan",
-            live:"https://online-store-pt8ruqtva-guillermoaviles.vercel.app/",
-            description: "An online store where users can buy and sell items. The application was built using MERN stack technologies and it has full CRUD actions."
-        },
-        {
-            id:3,
-            name:"Game of Thrones",
-            image: Project3,
-            github:"https://github.com/nghiavo24/game-of-thrones",
-            live:"https://game-of-thrones-nu.vercel.app/",
-            description: "A mini-wikipedia about game show Game of Thrones. It is a deep dive into ReactJS and parsing data from 3rd-part API to present it effectively. "
-        },
-        {
-            id:4,
-            name:"Did you know that?",
-            image: Project4,
-            github:"https://github.com/nghiavo24/Did-you-know-that",
-            live:"https://did-you-know-that.vercel.app/",
-            description: "A trivia game about Marvel. HTML, CSS and vanilla JavaScript were used to build this fun game."
-        },
-    
-    ]
-    
-  
   return (
     <div name='project' className='w-full h-fit text-black bg-[#DDD4D0] dark:text-white dark:bg-[#211a1d] xl:pt-12 xl:h-screen'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
